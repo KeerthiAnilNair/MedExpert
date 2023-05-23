ff9c2989bec45d463385aced0495145b8b4dc3ec
Devika-S-Kumar <devikaskumar29@gmail.com>
2023-05-23 21:50:45 +0530

    adding index

---
+++ index.js
@@ -0,0 +1,30 @@
+const b1 = document.querySelector('.b1');
+const b2 = document.querySelector('.b2');
+const b3 = document.querySelector('.b3');
+
+
+function f1(){
+          window.location.href = "#";
+}
+
+function f2(){
+          window.location.href = "#";
+}
+
+function f3(){
+          window.location.href = "#";
+}
+
+
+
+
+
+
+
+
+
+
+
+b1.addEventListener('click', f1);
+b2.addEventListener('click', f2);
+b3.addEventListener('click', f3);
